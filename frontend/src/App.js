f5dd169e3d2595a107cb642b7f2ec0ed368faa71
Simon-SS <simonschroeder00@gmail.com>
2021-05-06 22:39:29 -0400

    Added routes to product pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import { Container } from 'react-bootstrap'
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import logo from './logo.svg';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen'
+import logo from "./logo.svg";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen"
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-        <main className="py-3">
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen} exact/>
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
