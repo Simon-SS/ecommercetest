10e44ddc47bb35412299fde2b260c3edb62e6680
Simon-SS <simonschroeder00@gmail.com>
2021-05-09 20:13:49 -0400

    Added some more cart functionality, moving onto creating the actual cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import logo from "./logo.svg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen"
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path='/' component={HomeScreen} exact/>
-          <Route path='/product/:id' component={ProductScreen} />
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
